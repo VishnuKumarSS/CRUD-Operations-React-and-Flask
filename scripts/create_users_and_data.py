8c65cc608d08a8245c4c1ad85c9f8d126bf3589a
VishnuKumarSS <vishnukumarss941@gmail.com>
2024-02-29 02:17:14 +0530

    Script addition to create initial 50 users!

---
+++ scripts/create_users_and_data.py
@@ -0,0 +1,40 @@
+import requests
+import random
+
+
+cities = ["Madurai", "Chennai", "Bangalore", "Coimbatore"]
+
+for i in range(1, 51):
+    usertype = "admin" if i < 10 else "normal"
+    
+    email = f"{usertype}_user_{i}@gmail.com"
+    fullname = f"{usertype} user {i}"
+    password = f"{usertype}userpass{i}"
+
+    userage = random.randint(18, 30)
+    usercity = random.choice(cities)
+    username = f"{usertype}user{i}"
+    
+    create_user = requests.post(
+        "http://localhost:3000/create_user",
+        json={
+            "email": email,
+            "fullname": fullname,
+            "password": password
+        }
+    )
+    print("\n", "Create User:", create_user, create_user.__dict__, "\n")
+
+
+    update_user = requests.post(
+        "http://localhost:3000/add_user_data",
+        json={
+            "userage": userage,
+            "usercity": usercity,
+            "username": username,
+            "usertype": usertype 
+        }
+    )
+    print("\n", "Update User:", update_user, update_user.__dict__, "\n")
+
+    
